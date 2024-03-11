da7d85a6d859f6ab45a44bf59b101adff9e8aba0
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-03-10 19:51:00 -0700

    equal paths complete

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,39 @@
+#include <algorithm>
+#include <cstddef>
 #ifndef RECCHECK
-//if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+// if you want to add any #includes like <iostream> you must do them here
+// (before the next endif)
 #endif
 
 #include "equal-paths.h"
 using namespace std;
 
-
 // You may add any prototypes of helper functions here
 
-
-bool equalPaths(Node * root)
-{
-    // Add your code below
-
+int getHeight(Node *root) {
+  if (root == nullptr) {
+    return 0;
+  }
+  return 1 + max(getHeight(root->left), getHeight(root->right));
 }
 
+bool equalPaths(Node *root) {
+  // Add your code below
+
+  if (root == nullptr) {
+    return true;
+  }
+
+  int leftHeight = getHeight(root->left);
+  int rightHeight = getHeight(root->right);
+
+  if (leftHeight == 0) {
+    return equalPaths(root->right);
+  } else if (rightHeight == 0) {
+    return equalPaths(root->left);
+  } else if (leftHeight != rightHeight) {
+    return false;
+  }
+
+  return equalPaths(root->left) && equalPaths(root->right);
+}
